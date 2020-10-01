247c373a417d48b9cab22c3b5626c890bcc653ea
Lila Titus <ltitus@umich.edu>
2020-10-01 17:40:29 -0400

    updated

--- Hello.java
+++ Hello.java
@@ -8,13 +8,13 @@ public static void main (String[] args)
 for(int i=1; i<=10;i++)
 {
 
-system.out.println("Hello world..."+i);
+System.out.println("Hello world..."+i);
 //comment
 }
 //new comment
 
 //another comment
-system.out.println("Lab 4 CIS 285");
+System.out.println("Lab 4 CIS 285");
 }
 
 
